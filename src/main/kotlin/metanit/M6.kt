f2247982f4db876c6eabbb40456438ffdfcce706
Vladimir <ZabotinV78@gmail.com>
2025-11-19 18:22:51 +0500

    Повторил ещё 3 темы, а также javadoc

---
+++ src/main/kotlin/metanit/M6.kt
@@ -0,0 +1,17 @@
+package metanit
+
+/**
+ * В этом разделе описываются функции для ввода и вывода
+ * информации в строковом виде на консоль.
+ * @param println это функция вывода на консоль с переводом строки,
+ * ей не обязательно иметь входные параметры.
+ * @param print это функция вывода на консоль, требующая ввода.
+ * @param readln это функция ввода с консоли.
+ */
+fun main() {
+    println("Hello people!")
+    println()
+    print("Enter your name:\t")
+    val name = readln()
+    println("Hello $name!")
+}
