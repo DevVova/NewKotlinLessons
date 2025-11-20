f6c73e08aa35492cc5718029c80cea7347f6b42f
Vladimir <ZabotinV78@gmail.com>
2025-11-20 10:37:11 +0500

    Сегодня решил не напрягаться и пока вот только 2 темы повторил

---
+++ src/main/kotlin/metanit/M9.kt
@@ -0,0 +1,27 @@
+package metanit
+
+/**
+ * Условные конструкции позволяют направить выполнение программы по одному из путей в зависимости от условия.
+ */
+fun main() {
+    val a = 7
+    val b = 9
+    val c = a * 3
+    if (a == b) {
+        println("a = b")
+    } else if (a < b) println("a < b")
+    else println("a > b")
+
+    //Стоит отметить, что конструкция if может возвращать значение.
+    val d = if (c == b) {
+        println("c = b")
+        a
+    } else if (c < b) {
+        println("c < b")
+        b
+    } else {
+        println("c > b")
+        c
+    }
+    println("d = $d.")
+}
